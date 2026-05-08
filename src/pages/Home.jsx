ca22b0440b4a1be8ba9d0f8a9a1db34b1c8fe2cc
Mouloud78 <mouloudouldali78@gmail.com>
2026-05-07 21:27:40 -0400

    star background added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,6 @@
-import { ThemeToggle } from "../components/ThemeToggle";
+import { Star } from "lucide-react";
+import { ThemeToggle } from "@/components/ThemeToggle";
+import { StarBackground } from "@/components/StarBackground";
 
 export const Home = () => {
   return (
@@ -6,7 +8,7 @@ export const Home = () => {
       {/* Theme Toggle */}
       <ThemeToggle />
       {/* Background Effects */}
-
+      <StarBackground />
       {/* Navbar */}
 
       {/* Main Content */}
